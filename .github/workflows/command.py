d10437fc951946d7ec0409ce6306bea968edc947
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-21 15:09:39 +0530

    command use
    command basics

---
+++ .github/workflows/command.py
@@ -0,0 +1,22 @@
+Miniconda Distribution-smaller version of anaconda
+Miniconda-conda+python+few use ful libraries
+Anaconda -500mb(150+ scientific packages)
+Miniconda -50-75mb(miniconda distribution-https://docs.conda.io/en/latest/miniconda.html)
+
+
+
+//command lines basics
+  * command Prompt(windows)or Terminals is a command line interpreter to excecuted commands we enter.
+  * Allows to programmatically move around your computers directories/folders
+  
+list down the current directory
+  1)open start
+  2)type cmd
+  3)to know the current directory then,type cd.
+  
+  To list the all the files
+  command is dir
+  how to enetr into sub folders any folder like desktop the tyep : cd desktop( nm of folder),
+  it will show u the files in decktop.
+  how to go out  one step out of the folder command is cd...
+   To clear  all the folders use the command as  cls 
