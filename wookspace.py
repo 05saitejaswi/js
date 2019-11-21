d0d7e694bd89315349ca0ffba69e1e7c42392775
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-21 14:44:43 +0530

    python
    introduction of python

---
+++ wookspace.py
@@ -0,0 +1,32 @@
+Pythons idea originated in 1989 and released in 1991 by guido van Rossum
+python 2.0 -october,2000
+python 2.7.16 latest
+python 3.0 dec 2008
+python 3.7.4
+
+
+application of python
+1)machine learning
+2)Robotics
+3)Game Development
+4)Data Analysis
+5)Network programming
+6)web scraping
+
+
+
+Advantages:
+1) easy to learn
+2)readable- indentation is mandatory(to become more readable)
+3)free and open soursce
+4)Extensive libraries and frameworks
+* Django and Flask- web development
+* Numpy and Scipy-datascience
+* Pandas and scikit-learn-Machine learning
+
+
+
+Now a days python is used by many  companines like Facebook,Google,NASA,Dropbox,Yahoo,......
+
+//printing how is world
+print "how is  world"
