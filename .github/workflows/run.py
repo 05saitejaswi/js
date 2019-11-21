11b0aa47c290a808215dda97d52d4a66e94179ed
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-21 15:45:23 +0530

    print
    new line

---
+++ .github/workflows/run.py
@@ -0,0 +1,28 @@
+text-editors:
+*) any text editor that let you edits text
+*) Most of the text editors are desigined to be pytrhon specific
+*) sublimetext-most popular
+running python code
+IDE( integrated Development Enviroment):
+*) Special text editors that provides advanced editing,interactive testing and more features.
+*)Designed by group of developers
+*)
+python provides its default IDE called as 'IDE'" it will accept only one line "
+
+to write more than one line of code u have to use /,it tells that we are going to write more than one lines 
+example:
+>>> print("Hello world")
+hello world
+>>> for i in "Hello World":\
+print(i)
+h
+e
+l
+l
+o
+w
+o
+r
+l
+d
+>>>
