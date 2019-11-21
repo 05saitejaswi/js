ea7daecf486cd643200541b187a3341d277f1ab3
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-21 16:16:49 +0530

    var
    variables

---
+++ .github/workflows/var.py
@@ -0,0 +1,24 @@
+web-based interactive development enviroment
+supports rich text element
+most popular -jupyter notebook(it dont want internet)
+
+variables:-
+variables names can have letters ,numbers and undercores
+cannot start with numbers
+-eg: 1_student,
+spaces are not allowed
+Name s should be short and descriptive
+cannot use python functions /keywords
+like elif,if,import,def,true,none,etc...
+example:-
+          message="hello world"
+          print(message)
+  
+ when we make a mistake in the prg the prg shows "traceback"
+   **** the python will use the current data for the same variable *****   
+   example
+   message="hello"
+   message="hii"
+   print(message)
+   //answer will be hii (becoz it is a updated one)
+   
