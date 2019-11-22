862a7d4fe01dce3c2754f40312255a76deb4e88b
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-23 02:28:32 +0530

    modify
    add change delete

---
+++ .github/workflows/modify.py
@@ -0,0 +1,20 @@
+//modifying elements(change,add,remove)
+//change
+letter=['a','d','c']
+letter[0]='r'
+print(letter)
+['r','d','c']
+//add
+letter=['a','d','c']
+letter[2]='3'
+print(letter)
+['a','d',3]
+//delete
+letter=['a','d','c']
+letter[2]='2.1'
+print(letter)
+['a','2.1',3]
+
+
+//appending Element
+
