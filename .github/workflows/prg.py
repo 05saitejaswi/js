519f359d00745264518092364996c75f1a7c6bc1
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-23 02:05:03 +0530

    list
    list ,scope .... prg

---
+++ .github/workflows/prg.py
@@ -0,0 +1,57 @@
+//wishes someones birthday
+age= 21
+message ="happy" +str(age)+ " birthday"
+happy 21 birthday
+
+//lists
+List is a collection of iteams in a paticular order
+it stores a set of information which can be few iteams or millions of item
+SYNTAX:list_name=["item_1",iteam_2]
+ items of different data types
+ eg"\
+ test=[56,7.8."sad"]
+ 
+ //list of str data types
+   alpha=['as','asd']
+   print(alpha)
+   ['as','asd']
+   
+   //access individual iteams in a list
+   print(alpha[1])
+   //list starts from 0 to n
+   -1 means it will return last one
+      alpha=['as','asd']
+      -2 means it will points to last to second
+      
+      na,es=['sai','tej','tejaswi']
+      second_name=name[1]
+      print(second_name)
+      tej
+      print('your name is' + second_name)
+      your name is tej
+      
+      //slicing Element in list
+      syntax:list_name[start:stop]
+      letters=['a','c','d']
+      letters[0:2]
+      ['a','c']
+      letter[2:3]
+      ['d']
+      letter[::]//print all meaning ::
+      ['a','c','d']
+      letter[::2]
+      ['a','c','d']
+      
+      name = 'saitejaswi'// starts number as  0,1,2,3,.....
+       print(name[3])
+       t
+       name[2:]
+       tejaswi
+       name[::-1]//reverse the string
+       iwsajetias
+       
+       
+       
+       
+      
+      
