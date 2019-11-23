bb7de85055b87d2fcf873a413992521bc9120811
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-23 23:51:07 +0530

    appendent
    append

---
+++ .github/workflows/append.py
@@ -0,0 +1,36 @@
+use of append
+appending at the end
+letter=['a','d','f','g']
+letter.append('u')
+print(letter)
+['a','d','f','g','u']
+
+new_letter=[]
+print(new_letter)
+[]
+
+new_letter.append('d')
+new_lettre.append('f')
+new_letter.append('j')
+new_lettre.append('k')
+new_letter.append('l')
+new_lettre.append('w')
+print (new_letter)
+['a','d','f','j','k','l','w']
+
+
+//insert element
+letter=['a','d','f','j','k','l','w']
+
+letter.insert(2,'f')
+['a','d','f','j','k','l','w']
+
+//deletion number
+letter=['a','d','f']
+del.letter[0]
+['d','f']
+
+
+
+
+
