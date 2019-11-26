03127a49fcbb45f88c6a0ba8753245c900c4ab7c
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-26 23:41:21 +0530

    num
    numerical list

---
+++ .github/workflows/numericlist.py
@@ -0,0 +1,18 @@
+for value in range(1,5)
+print(value)
+1
+2
+3
+4
+5
+
+//range to make list of numbers
+numbers=list(range(1,6))
+print(numbers)
+[1,2,3,4,5]
+
+//to print the number till 15 by add +2 to all numbers
+numbers=list(range(1,15,2))
+print(numbers)
+[1,3,5,7,9,11,13]
+
