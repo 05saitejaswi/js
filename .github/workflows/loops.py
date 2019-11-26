c6c1f07b3a37152f1d4cdcccf3999318106d1c0b
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2019-11-26 23:23:54 +0530

    loop
    loops

---
+++ .github/workflows/loops.py
@@ -0,0 +1,10 @@
+list_name=['tom','jack','jill']
+for name in list_name:
+ print(name)//giving a tap space is called indentation which will cause an error
+tom
+jack
+jill
+print(name+",welcome to python")
+tom,welcome to python coursce
+jack,welcome to python coursce
+jill,welcome to python coursce
